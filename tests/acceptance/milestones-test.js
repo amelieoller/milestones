252ce7a84d5a546ec7d296569d34db2f32948938
Amelie Oller <amelieoller@gmail.com>
2020-09-14 21:09:02 -0700

    Add navbar to all pages and create home

--- tests/acceptance/milestones-test.js
+++ tests/acceptance/milestones-test.js
@@ -9,11 +9,34 @@ module('Acceptance | milestones', function (hooks) {
     await visit('/about');
 
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
     assert.dom('h1').hasText('About Milestones');
 
     assert.dom('a[href="/"]').hasText('Home');
     await click('a[href="/"]');
 
     assert.equal(currentURL(), '/');
+  });
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Your Milestones');
+  });
+
+  test('navigating using the navbar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav .menu-home').hasText('Home');
+    assert.dom('nav .menu-about').hasText('About');
+
+    await click('nav .menu-home');
+    assert.equal(currentURL(), '/');
+
+    await click('nav .menu-about');
+    assert.equal(currentURL(), '/about');
   });
 });
